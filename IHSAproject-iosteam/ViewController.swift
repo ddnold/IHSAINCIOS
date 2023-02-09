b2e549aa635320aada165e3818dfb6d2e9793a09
Dustin Nold <s532534@nwmissouri.edu>
2023-02-09 15:20:25 -0600

    Changed main to new searchbar

--- IHSAproject-iosteam/ViewController.swift
+++ IHSAproject-iosteam/ViewController.swift
@@ -6,37 +6,62 @@
 //
 
 import UIKit
-import DropDown
 
 class ViewController: UIViewController {
     
     //ihsalabel
     @IBOutlet weak var ihsaImg: UIImageView!
-    @IBOutlet weak var myDropDownView: UIView!
-    @IBOutlet weak var dropDownButton: UIButton!
-    @IBOutlet weak var showLabel: UILabel!
+    @IBOutlet weak var searchBar: UISearchBar!
     
-    let myDropDown = DropDown()
-    var shows = ["show1","show2","show3"]
+    @IBOutlet weak var tableView: UITableView!
     
-    @IBAction func isTappedDropDownButton(_ sender: Any) {
-        myDropDown.show()
-    }
-    
+    let data = ["show1","show2","show3","show4"]
+    var filteredData: [String]!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        myDropDown.anchorView = myDropDownView
-        myDropDown.dataSource = shows
-        
-        myDropDown.bottomOffset = CGPoint(x: 0, y: (myDropDown.anchorView?.plainView.bounds.height)!)
-        myDropDown.topOffset = CGPoint(x: 0, y: -(myDropDown.anchorView?.plainView.bounds.height)!)
-        myDropDown.direction = .bottom
-        
-        myDropDown.selectionAction = { (index: Int, item: String) in self.showLabel.text = self.shows[index]
-            self.showLabel.textColor = .black
-            print("index \(index) and \(item)")
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
+        filteredData = data
+
         }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return filteredData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for:indexPath)
+        cell.textLabel?.text = filteredData[indexPath.row]
+        return cell
+    }
+    
+}
+
+extension ViewController: UISearchBarDelegate{
+    
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        self.tableView.isHidden = false
+        filteredData = []
+        if searchText == ""
+        {
+            filteredData = data
+        }
+        for word in data{
+            if word.uppercased().contains(searchText.uppercased()){
+                filteredData.append(word)
+            }
+        }
+        if filteredData.isEmpty{
+            filteredData = data
+        }
+        self.tableView.reloadData()
+        
     }
 }
     
