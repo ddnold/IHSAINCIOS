2bea4c631f87d7edd488da0a446e81c4ed2298ee
Dustin Nold <s532534@nwmissouri.edu>
2023-02-14 14:59:15 -0600

    Revert "removed junk"
    This reverts commit c094f5a3e2271451988dfb6236a150cb21ed55f3.

---
+++ IHSAproject-iosteam/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  IHSAproject-iosteam
+//
+//  Created by student on 1/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //ihsalabel
+    @IBOutlet weak var ihsaImg: UIImageView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let data = ["show1","show2","show3","show4"]
+    var filteredData: [String]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
+        filteredData = data
+
+        }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return filteredData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for:indexPath)
+        cell.textLabel?.text = filteredData[indexPath.row]
+        return cell
+    }
+    
+}
+
+extension ViewController: UISearchBarDelegate{
+    
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        self.tableView.isHidden = false
+        filteredData = []
+        if searchText == ""
+        {
+            filteredData = data
+        }
+        for word in data{
+            if word.uppercased().contains(searchText.uppercased()){
+                filteredData.append(word)
+            }
+        }
+        if filteredData.isEmpty{
+            filteredData = data
+        }
+        self.tableView.reloadData()
+        
+    }
+}
+    
+    
+    
+
+
+
