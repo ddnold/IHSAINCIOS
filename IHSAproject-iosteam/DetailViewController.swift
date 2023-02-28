ea9881f802284cb6ae6ef3147f74bb5aebe8fd9f
s533294 <89223971+s533294@users.noreply.github.com>
2023-02-28 13:48:28 -0600

    DetailView Formatting/Cleaned Up

--- IHSAproject-iosteam/DetailViewController.swift
+++ IHSAproject-iosteam/DetailViewController.swift
@@ -3,40 +3,11 @@
 //  IHSAproject-iosteam
 //
 //  Created by Nold,Dustin J on 2/21/23.
-//
+// Modified, Committed, Pushed by Carter Smith, 2/28/2023
 
 import UIKit
 
 class DetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return filteredData.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for:indexPath)
-        cell.textLabel?.text = filteredData[indexPath.row]
-        return cell
-    }
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
-        self.tableViewOutlet.isHidden = false
-        filteredData = []
-        if searchText == ""
-        {
-            filteredData = data
-        }
-        for word in data{
-            if word.uppercased().contains(searchText.uppercased()){
-                filteredData.append(word)
-            }
-        }
-        if filteredData.isEmpty{
-            filteredData = data
-        }
-        self.tableViewOutlet.reloadData()
-        
-    }
-    
     
     @IBOutlet weak var titleOutlet: UILabel!
     
@@ -51,11 +22,20 @@ class DetailViewController: UIViewController, UITableViewDataSource, UITableView
     @IBOutlet weak var tableViewOutlet: UITableView!
     
     let data = ["Rider One, a School","Rider Two, another School","Rider Three, some other School","Rider Four, a School","Rider Five,  another School"]
+    
+    let date:String = "February 28, 2023"
+    let time:String = "10:00 AM"
+    let location:String = "1234 1st St. City, State, ZIP"
+    
     var filteredData: [String]!
     
     override func viewDidLoad() {
-        titleOutlet.text! = title!
         super.viewDidLoad()
+        
+        titleOutlet.text! = title!
+        dateOutlet.text! = date
+        timeOutlet.text! = time
+        locationOutlet.text! = location
          
         filteredData = data
         
@@ -81,6 +61,47 @@ class DetailViewController: UIViewController, UITableViewDataSource, UITableView
         // Pass the selected object to the new view controller.
     }
     */
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for:indexPath)
+        cell.textLabel?.text = filteredData[indexPath.row]
+        return cell
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
+        self.tableViewOutlet.isHidden = false
+        filteredData = []
+        if searchText == ""
+        {
+            filteredData = data
+        }
+        for word in data{
+            if word.uppercased().contains(searchText.uppercased()){
+                filteredData.append(word)
+            }
+        }
+        if filteredData.isEmpty{
+            filteredData = data
+        }
+        self.tableViewOutlet.reloadData()
+    }
+    
+    /*
+     //Rider View Controller setup
+     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            // Get the selected cell and its text label
+            let cell = tableView.cellForRow(at: indexPath)
+            guard let textLabel = cell?.textLabel else { return }
+            
+            // Show a new view with the selected text as the title
+            guard let riderViewController = storyboard?.instantiateViewController(withIdentifier: "RiderViewController") as? RiderViewController else { return }
+        riderViewController.title = textLabel.text
+        navigationController?.pushViewController(riderViewController, animated: true)
+    }
+     */
+    
 
 }
 
