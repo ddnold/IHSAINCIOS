58fe64c2423fd196988cf45a76110181053e8358
s533294 <89223971+s533294@users.noreply.github.com>
2023-02-28 13:16:15 -0600

    DetailViewController Search
    Can filter by rider/school

--- IHSAproject-iosteam/DetailViewController.swift
+++ IHSAproject-iosteam/DetailViewController.swift
@@ -7,7 +7,36 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for:indexPath)
+        cell.textLabel?.text = filteredData[indexPath.row]
+        return cell
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
+        self.tableViewOutlet.isHidden = false
+        filteredData = []
+        if searchText == ""
+        {
+            filteredData = data
+        }
+        for word in data{
+            if word.uppercased().contains(searchText.uppercased()){
+                filteredData.append(word)
+            }
+        }
+        if filteredData.isEmpty{
+            filteredData = data
+        }
+        self.tableViewOutlet.reloadData()
+        
+    }
+    
     
     @IBOutlet weak var titleOutlet: UILabel!
     
@@ -21,12 +50,20 @@ class DetailViewController: UIViewController {
     
     @IBOutlet weak var tableViewOutlet: UITableView!
     
-    let data = ["riderOne, a School","riderTwo, another School","riderThree, some other School","riderFour, a School","riderFive,  another School"]
+    let data = ["Rider One, a School","Rider Two, another School","Rider Three, some other School","Rider Four, a School","Rider Five,  another School"]
     var filteredData: [String]!
     
     override func viewDidLoad() {
         titleOutlet.text! = title!
         super.viewDidLoad()
+         
+        filteredData = data
+        
+        tableViewOutlet.delegate = self
+        tableViewOutlet.dataSource = self
+        
+        searchOutlet.delegate = self
+        
         filteredData = data
         tableViewOutlet.backgroundColor = .white // or any other color
         tableViewOutlet.bounces = true
