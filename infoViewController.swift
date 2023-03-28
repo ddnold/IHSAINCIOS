55d44954057759c0006e020f8518158d45440199
Haydon Sackett <77422080+haydonsackett@users.noreply.github.com>
2023-03-28 13:11:31 -0500

    Info View

--- infoViewController.swift
+++ infoViewController.swift
@@ -12,11 +12,13 @@ class infoViewController: UIViewController {
     @IBOutlet weak var myLabel2: UILabel!
     
     //ihsalogo
-    @IBOutlet weak var ihsaLogo: UIImageView!
+    @IBOutlet weak var infoimg: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //creating url for first label; ISHA website
+        let infoImageView = UIImageView(frame: CGRect(x:0, y:0, width: 200, height: 200))
+        infoimg.image = UIImage(named: "image1")
         let attributedString = NSMutableAttributedString(string: "IHSA website")
         let url = URL(string: "https://www.ihsainc.com/about-us/general-information")!
         let range = NSRange(location: 0, length: attributedString.length)
@@ -28,6 +30,5 @@ class infoViewController: UIViewController {
         let range2 = NSRange(location: 0, length: attributedString2.length)
         attributedString2.addAttribute(.link, value: url2, range: range2)
         myLabel2.attributedText = attributedString2
-        ihsaLogo.image = UIImage(named: "image1")
     }
 }
