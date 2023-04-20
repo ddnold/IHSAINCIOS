ad271b419a7bf54a5dac213a2945e317eb426d8d
Dustin Nold <s532534@nwmissouri.edu>
2023-04-20 15:43:50 -0500

    Got local favorites added and working as intended.
    Currently just using temporary data and removing the item from the users local array. This local array still needs to be stored locally on the users device. Additionally, we need functionality that can add to this list from the other "remoteFavoritesViewController" scene.

--- IHSAproject-iosteam/LocalFavoritesViewController.swift
+++ IHSAproject-iosteam/LocalFavoritesViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 struct Rider {
     let name: String
+    let school: String
     let id: Int
 }
 
@@ -18,61 +19,67 @@ var userRiders: [Rider] = []
 class LocalFavoritesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
     
     @IBOutlet weak var tableView: UITableView!
-
-    @IBAction func removeRiderButtonTapped(_ sender: UIButton) {
-        let cell = sender.superview?.superview as! UITableViewCell
-        if let indexPath = tableView.indexPath(for: cell) {
-            userRiders.remove(at: indexPath.row)
-            // Remove rider from database or other data storage here
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        }
-    }
     
+    @IBAction func removeRiderButtonTapped(_ sender: UIButton) {
+        var superview = sender.superview
+        while let view = superview, !(view is UITableViewCell) {
+            superview = view.superview
+        }
+        guard let cell = superview as? UITableViewCell,
+              let indexPath = tableView.indexPath(for: cell) else {
+            return
+        }
+        userRiders.remove(at: indexPath.row)
+        // Remove rider from database or other data storage here
+        tableView.deleteRows(at: [indexPath], with: .fade)
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return userRiders.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "RiderTableViewCell", for: indexPath) as! RiderTableViewCell
-        let rider = userRiders[indexPath.row]
-        cell.nameLabel?.text = rider.name
-        cell.addButton?.tag = indexPath.row
-        cell.addButton?.addTarget(self, action: #selector(removeRiderButtonTapped(_:)), for: .touchUpInside)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for:indexPath)
+        //cell.textLabel?.text = filteredData[indexPath.row]
+        let riders = userRiders[indexPath.row]
+        
+        //get element with tag 2001, set rider name
+        if let name = cell.contentView.viewWithTag(3000) as? UILabel {
+            name.text = riders.name
+        }
+        //get element with tag 2002, set rider name
+        if let school = cell.contentView.viewWithTag(3001) as? UILabel {
+            school.text = riders.school
+        }
+        if let button = cell.contentView.viewWithTag(3002) as? UIButton {
+            button.tag = indexPath.row
+            button.addTarget(self, action: #selector(removeRiderButtonTapped(_:)), for: .touchUpInside)
+        }
         return cell
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // Your code for handling row selection goes here
-    }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.register(RiderTableViewCell.self, forCellReuseIdentifier: "RiderTableViewCell")
-
-        
         tableView.dataSource = self
         tableView.delegate = self
+        tableView.isEditing = false
         
-        let rider1 = Rider(name: "John", id: 1)
-        let rider2 = Rider(name: "Jane", id: 2)
-        userRiders = [rider1, rider2]
-        print(userRiders)
+        let rider1 = Rider(name: "John", school: "Northwest", id: 1)
+        let rider2 = Rider(name: "Jane", school: "Midwest", id: 2)
+        let rider3 = Rider(name: "JohnD", school: "Northwest", id: 1)
+        let rider4 = Rider(name: "JaneD", school: "Midwest", id: 2)
+        userRiders = [rider1, rider2, rider3, rider4]
         tableView.reloadData()
         
         
-
+        
     }
-
-
-
-
-}
-
-
-class RiderTableViewCell: UITableViewCell {
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var addButton: UIButton!
+    
+    
+    
+    
 }
 
 
